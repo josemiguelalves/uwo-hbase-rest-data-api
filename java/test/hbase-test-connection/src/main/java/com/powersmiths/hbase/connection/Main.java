c5db19849d5f15b43b539b49e22f9a229829e0ed
Jose Miguel <jose.miguel.usp@gmail.com>
2018-05-14 16:15:17 -0400

    commit

--- java/test/hbase-test-connection/src/main/java/com/powersmiths/hbase/connection/Main.java
+++ java/test/hbase-test-connection/src/main/java/com/powersmiths/hbase/connection/Main.java
@@ -3,7 +3,10 @@
         import java.io.IOException;
 
 
+        import com.google.common.base.Stopwatch;
         import com.mongodb.client.FindIterable;
+        import com.powersmiths.DAO.RealtimeDAO;
+        import com.powersmiths.data.Realtime;
         import org.apache.hadoop.conf.Configuration;
         import org.apache.hadoop.hbase.HBaseConfiguration;
         import org.apache.hadoop.hbase.TableName;
@@ -31,12 +34,7 @@
         import java.sql.Timestamp;
         import java.text.DateFormat;
         import java.text.SimpleDateFormat;
-        import java.util.Date;
-        import java.util.HashMap;
-        import java.util.Locale;
-        import java.util.Map;
-
-
+        import java.util.*;
 
 
         import com.mongodb.MongoClient;
@@ -47,7 +45,6 @@
         import com.mongodb.client.MongoCollection;
 
         import org.bson.Document;
-        import java.util.Arrays;
         import com.mongodb.Block;
 
         import com.mongodb.client.MongoCursor;
@@ -69,106 +66,94 @@
         public class Main {
 
             private static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                public static void HbaseConnection() {
+            private static final String columnName = "data_sumary";
+            private static final String columnValue = "value";
+
+            public static void HbaseConnection() {
 
 
-                    Table table;
-                    ResultScanner rs;
-                    Scan scan = new Scan();
-                    String rowKey = null;
-                    Get theGet;
-                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Table table;
+                ResultScanner rs;
+                Scan scan = new Scan();
+                String rowKey = null;
+                Get theGet;
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-                    Connection conn = null;
-
-
-                    Configuration config = HBaseConfiguration.create();
-                    config.set("hbase.zookeeper.quorum", "129.100.174.152");
-                    config.set("hbase.zookeeper.property.clientPort", "2181");
-                    config.set("hbase.master", "129.100.174.152:16000");
-                    config.set("hbase.client.retries.number", Integer.toString(3));
-                    config.set("zookeeper.session.timeout", Integer.toString(60000));
-                    config.set("zookeeper.recovery.retry", Integer.toString(3));
-                    config.set("hbase.cluster.distributed", "true");
-                    config.set("hbase.hbase.regionserver", "129.100.174.152:16023, 129.100.174.152:16022, 129.100.174.152:16021, 129.100.174.152:16029");
+                Connection conn = null;
 
 
-
-                    try
-                    {
-
-                        conn = ConnectionFactory.createConnection(config);
-                        System.out.println("Master info port: "+conn.getAdmin().getConfiguration().toString());
-                        System.out.println("GetConfiguration: "+conn.getConfiguration().toString());
-                    }
-                    catch (IOException ex)
-                    {
-                        System.out.println("IOException : " + ex.getMessage());
-                    }
-                    catch (NoClassDefFoundError ex)
-                    {
-                        System.out.println("Error : " + ex.getMessage());
-                    }
+                Configuration config = HBaseConfiguration.create();
+                config.set("hbase.zookeeper.quorum", "129.100.174.152");
+                config.set("hbase.zookeeper.property.clientPort", "2181");
+                config.set("hbase.master", "129.100.174.152:16000");
+                config.set("hbase.client.retries.number", Integer.toString(3));
+                config.set("zookeeper.session.timeout", Integer.toString(60000));
+                config.set("zookeeper.recovery.retry", Integer.toString(3));
+                config.set("hbase.cluster.distributed", "true");
+                config.set("hbase.hbase.regionserver", "129.100.174.152:16023, 129.100.174.152:16022, 129.100.174.152:16021, 129.100.174.152:16029");
 
 
+                try {
 
-                    long id = 0L;
-                    String date_str = null;
-                    DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH", Locale.ENGLISH);
-
-                    String date_day_str = null;
-
-                    try
-                    {
-                        table = conn.getTable(TableName.valueOf("powersmiths:readings_hour"));
-                        rs = table.getScanner(scan);
-
-                        for (Result res : rs)
-                        {
-                            id += 1L;
-
-                            rowKey = Bytes.toString(res.getRow());
-                            String rowKey_name = getSubstringUntilFirstNumber(rowKey).trim();
-
-                            date_str = rowKey.substring(rowKey.length() - 13);
-                            Date date_day = format.parse(date_str);
-                            date_day_str  = df.format(date_day);
-
-                            String value = Bytes.toString(res.getValue(Bytes.toBytes("data_summary"), Bytes.toBytes("value")));
-                            BigDecimal num = new BigDecimal(value);
-                            num = num.setScale(2, BigDecimal.ROUND_CEILING);
-                            String valueWithNoExponents = num.toPlainString();
-
-                            // days.put(Long.valueOf(id), new Day(id, rowKey_name, "not avaiable", date_day_str, valueWithNoExponents));
-
-        //
-        //
-        //				System.out.println("value: " + name);
-
-                            System.out.println("rowKey_name: " + rowKey_name + " date_day_str: " + date_day_str +  " value: " + value);
-
-                        }
-                        rs.close();
-                    }
-                    catch (Exception e)
-                    {
-                        e.printStackTrace();
-                    }
-
-
-
+                    conn = ConnectionFactory.createConnection(config);
+                    System.out.println("Master info port: " + conn.getAdmin().getConfiguration().toString());
+                    System.out.println("GetConfiguration: " + conn.getConfiguration().toString());
+                } catch (IOException ex) {
+                    System.out.println("IOException : " + ex.getMessage());
+                } catch (NoClassDefFoundError ex) {
+                    System.out.println("Error : " + ex.getMessage());
                 }
 
 
-            private static  String getSubstringUntilFirstNumber(String source)
-            {
+                long id = 0L;
+                String date_str = null;
+                DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH", Locale.ENGLISH);
+
+                String date_day_str = null;
+
+                try {
+                    table = conn.getTable(TableName.valueOf("powersmiths:readings_hour"));
+                    rs = table.getScanner(scan);
+
+                    for (Result res : rs) {
+                        id += 1L;
+
+                        rowKey = Bytes.toString(res.getRow());
+                        String rowKey_name = getSubstringUntilFirstNumber(rowKey).trim();
+
+                        date_str = rowKey.substring(rowKey.length() - 13);
+                        Date date_day = format.parse(date_str);
+                        date_day_str = df.format(date_day);
+
+                        String value = Bytes.toString(res.getValue(Bytes.toBytes("data_summary"), Bytes.toBytes("value")));
+                        BigDecimal num = new BigDecimal(value);
+                        num = num.setScale(2, BigDecimal.ROUND_CEILING);
+                        String valueWithNoExponents = num.toPlainString();
+
+                        // days.put(Long.valueOf(id), new Day(id, rowKey_name, "not avaiable", date_day_str, valueWithNoExponents));
+
+                        //
+                        //
+                        //				System.out.println("value: " + name);
+
+                        System.out.println("rowKey_name: " + rowKey_name + " date_day_str: " + date_day_str + " value: " + value);
+
+                    }
+                    rs.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+
+            private static String getSubstringUntilFirstNumber(String source) {
                 return source.split("(\\d{4}-\\d{2})")[0];
             }
 
 
-
-
-            public static void MongoConnection() {
+            public static void MongoConnectionDay() {
 
                 MongoClient mongoClient = new MongoClient( "129.100.174.152" , 30021 );
                 MongoDatabase database = mongoClient.getDatabase("powersmiths");
@@ -180,11 +165,9 @@
                 Scan scan = new Scan();
                 String date_day_str = null;
                 String rowKey = null;
-                Double value;
+                String value;
 
 
-//                MongoCursor<Document> cursor = collection.find().iterator();
-//
                 FindIterable<Document> results = collection.find();
 
                 try {
@@ -193,7 +176,7 @@
 
                     for (Document doc : results) {
                         rowKey = doc.getString("data_sumary");
-                        value = doc.getDouble("value");
+                        value = doc.getString("value");
 
                         String rowKey_name = getSubstringUntilFirstNumber(rowKey).trim();
 
@@ -202,38 +185,18 @@
                         date_day_str  = df.format(date_day);
 
 
+
                         BigDecimal num = new BigDecimal(value);
                         num = num.setScale(2, BigDecimal.ROUND_CEILING);
                         String valueWithNoExponents = num.toPlainString();
-                        System.out.println(rowKey+"   "+rowKey_name+"   "+date_day_str+"  "+valueWithNoExponents);
+
+
+
+                     System.out.println(rowKey+"   "+rowKey_name+"   "+date_day_str+"  "+valueWithNoExponents);
+
+
+
                     }
-
-
-
-//
-//                    while (cursor.hasNext()) {
-//                        rowKey = cursor.next().getString("data_sumary");
-//                        value = cursor.next().getDouble("value");
-//
-//                        String rowKey_name = getSubstringUntilFirstNumber(rowKey).trim();
-//
-//                        date_str = rowKey.substring(rowKey.length() - 13);
-//                        Date date_day = format.parse(date_str);
-//                        date_day_str  = df.format(date_day);
-//
-//
-//                        BigDecimal num = new BigDecimal(value);
-//                        num = num.setScale(2, BigDecimal.ROUND_CEILING);
-//                        String valueWithNoExponents = num.toPlainString();
-
-
-
- //                       System.out.println(rowKey+"   "+rowKey_name+"   "+date_day_str+"  "+valueWithNoExponents);
-
-
-
-                        //System.out.println(cursor.next().toJson());
- //                   }
                 }
 
                 catch (Exception e)
@@ -274,13 +237,170 @@
                 }
 
 
+            public static void MongoConnectionDayNameDate() {
+
+
+//                String date = null;
+//                String name = null;
+                MongoClient mongoClient = new MongoClient("129.100.174.152", 30021);
+                MongoDatabase database = mongoClient.getDatabase("powersmiths");
+                MongoCollection<Document> collection = database.getCollection("readings_hour");
+                long id = 0L;
+                String date_str = null;
+                DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH", Locale.ENGLISH);
+                String date_day_str = null;
+                String rowKey = null;
+                String value;
+                String name_query;
+
+//                name_query = name + " " + date;
+                name_query = "WindDirection2018-01-03";
+
+
+
+                FindIterable<Document> results = collection.find(Filters.regex("data_sumary", name_query));
+
+                try {
+
+
+                    for (Document doc : results) {
+                        rowKey = doc.getString("data_sumary");
+                        value = doc.getString("value");
+
+                        String rowKey_name = getSubstringUntilFirstNumber(rowKey).trim();
+
+                        date_str = rowKey.substring(rowKey.length() - 13);
+                        Date date_day = format.parse(date_str);
+                        date_day_str = df.format(date_day);
+
+
+                        BigDecimal num = new BigDecimal(value);
+                        num = num.setScale(2, BigDecimal.ROUND_CEILING);
+                        String valueWithNoExponents = num.toPlainString();
+                        System.out.println(rowKey + "   " + rowKey_name + "   " + date_day_str + "  " + valueWithNoExponents);
+                    }
+
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+
+
+            public static void scan() {
+
+                String date = null;
+                String name = null;
+                MongoClient mongoClient = new MongoClient("129.100.174.152", 30021);
+                MongoDatabase database = mongoClient.getDatabase("powersmiths");
+                MongoCollection<Document> collection = database.getCollection("readings_hour");
+
+            float val = 0;
+           FindIterable<Document> results = null;
+
+
+
+
+        try
+
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.start();
+
+                results = collection.find();
+                stopwatch.stop();
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                System.out.println("read;" + timestamp + ";" + String.valueOf(stopwatch.elapsedMillis()));
+
+            }
+        catch(
+            Exception e)
+
+            {
+                e.printStackTrace();
+            }
+
+        if(results !=null)
+
+            {
+                String value = String.valueOf(collection.count());
+                val = Float.valueOf(value);
+            }
+
+        System.out.print(val);
+
+        }
+
+            public static void insertMany()  {
+
+
+                MongoClient mongoClient = new MongoClient( "129.100.174.152" , 30021 );
+
+                MongoDatabase database = mongoClient.getDatabase("powersmiths");
+                MongoCollection<Document> collection = database.getCollection("readings_hour");
+
+
+
+                List<Document> documents = new ArrayList<Document>();
+                for (int i = 0; i < 10; i++) {
+                    documents.add(new Document("data_sumary", "WindDirection2018-01-0"+i+" 01").append( "value", String.valueOf(Math.random()*100)));
+                }
+
+
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.start();
+                collection.insertMany(documents);
+
+                stopwatch.stop();
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                System.out.println("write;" + timestamp + ";" + String.valueOf(stopwatch.elapsedMillis()));
+
+            }
+
+
+        public static void insert(Realtime record)  {
+
+
+            MongoClient mongoClient = new MongoClient( "129.100.174.152" , 30021 );
+
+            MongoDatabase database = mongoClient.getDatabase("powersmiths");
+            MongoCollection<Document> collection = database.getCollection("readings_hour");
+
+            String skey  = record.getKey();
+            String svalue = String.valueOf(record.getValue());
+
+            Document doc = new Document(columnName, skey)
+                    .append(columnValue, svalue);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.start();
+            collection.insertOne(doc);
+            stopwatch.stop();
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            System.out.println("write;" + timestamp + ";" + String.valueOf(stopwatch.elapsedMillis()));
+
+            }
+
 
             @SuppressWarnings("deprecation")
             public static void main(String[] args) throws ServiceException, MasterNotRunningException, ZooKeeperConnectionException, IOException {
 
-//              HbaseConnection();
-               MongoConnection();
+//             HbaseConnection();
+ //            MongoConnectionDay();
+//            MongoConnectionDayNameDate();
+           scan();
 
+
+                Realtime test_record = new Realtime();
+                test_record.setKey("WindDirection2018-01-20 22");
+                float test = (float)22.45;
+//                test_record.setValue(test);
+//                insert(test_record);
+
+ //               insertMany();
+   //             MongoConnectionDay();
             }
 
         }
